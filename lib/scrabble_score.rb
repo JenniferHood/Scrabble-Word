ee074f1f6f2d5ce82e032527adf383ba7796e27d
Ben Cornelis & Jeremy Greenwood <student@epicodus.com>
2015-04-21 13:25:31 -0700

    It will work for any word.

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -4,16 +4,11 @@ class String
       ['d', 'g'] => 2, ['b','c','m','p'] => 3, ['f','h','v','w','y'] => 4,
       ['k'] => 5, ['j', 'x'] => 8, ['q','z'] =>10}
     total_score = 0
-    first_letter = self[0]
-    second_letter = self[1]
-    scrabble_dict.each() do |letters, score|
-        if letters.include?(first_letter)
+    self.split('').each() do |letter|
+      scrabble_dict.each() do |letters, score|
+        if letters.include?(letter)
           total_score = total_score + score
         end
-    end
-    scrabble_dict.each() do |letters, score|
-      if letters.include?(second_letter)
-        total_score = total_score + score
       end
     end
     total_score
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -10,4 +10,8 @@ describe("String#scrabble") do
   it("returns a scrabble score for a two letter word") do
     expect("be".scrabble()).to(eq(4))
   end
+
+  it("returns a scrabble score for a word of any length") do
+    expect("spring".scrabble()).to(eq(9))
+  end
 end
