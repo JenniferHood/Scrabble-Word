d0e6b22a3f7d361942094b9882ad827682847c23
Ben Cornelis & Jeremy Greenwood <student@epicodus.com>
2015-04-21 12:19:32 -0700

    It will work for two letter words.

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -3,10 +3,19 @@ class String
     scrabble_dict = {['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'] => 1,
       ['d', 'g'] => 2, ['b','c','m','p'] => 3, ['f','h','v','w','y'] => 4,
       ['k'] => 5, ['j', 'x'] => 8, ['q','z'] =>10}
+    total_score = 0
+    first_letter = self[0]
+    second_letter = self[1]
     scrabble_dict.each() do |letters, score|
-        if letters.include?(self)
-          return score
+        if letters.include?(first_letter)
+          total_score = total_score + score
         end
     end
+    scrabble_dict.each() do |letters, score|
+      if letters.include?(second_letter)
+        total_score = total_score + score
+      end
+    end
+    total_score
   end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,4 +6,8 @@ describe("String#scrabble") do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble()).to(eq(1))
   end
+
+  it("returns a scrabble score for a two letter word") do
+    expect("be".scrabble()).to(eq(4))
+  end
 end
